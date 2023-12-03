2e392723f22c6601cdc38600893f06e3b85eed10
James Olsen <olsenjames1116@gmail.com>
2023-12-03 12:50:48 -0500

    Display form view from new route

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
 const indexRouter = require('./routes/index');
+const newRouter = require('./routes/new');
 const usersRouter = require('./routes/users');
 
 const app = express();
@@ -21,6 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/new', newRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
