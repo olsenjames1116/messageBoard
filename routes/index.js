7ef2dcfc8c206cc132aa91205d163330b3251f31
James Olsen <olsenjames1116@gmail.com>
2023-12-03 13:44:17 -0500

    Display a new message after it has been posted
    A new message is added to the index page after being posted from the new
    page so the user can see added messages. The new page can be routed to
    from a link on the index page to allow easier navigation.

--- routes/index.js
+++ routes/index.js
@@ -19,4 +19,11 @@ router.get('/', function (req, res, next) {
 	res.render('index', { title: 'Mini Message Board', messages: messages });
 });
 
+router.post('/new', function (req, res, next) {
+	const { author, message } = req.body;
+	messages.push({ text: message, user: author, added: new Date() });
+
+	res.redirect('/');
+});
+
 module.exports = router;
